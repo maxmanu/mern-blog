634dfa2ef54daba56809b5924e5ce104ae41b659
max <maxcamina@gmail.com>
2024-02-06 04:03:30 -0500

    add functionality to the sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import SignUp from './pages/SignUp'
-import SignOut from './pages/SignOut'
-import Header from './components/Header'
-import About from './pages/About'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
+import About from "./pages/About";
 
 export default function App() {
   return (
@@ -15,10 +15,10 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }

24a0c905d96fa19c3a8cc1152c53c1661a09d99d
max <maxcamina@gmail.com>
2024-02-06 03:29:31 -0500

    create sign up ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
-import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 import SignOut from './pages/SignOut'
 import Header from './components/Header'
 import About from './pages/About'
@@ -14,8 +14,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-out" element={<SignOut />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
